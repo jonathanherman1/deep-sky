ea530a48c16b233248cc7f91c08fddf7a2f9e77c
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-07-16 19:50:05 -0400

    Fix bug: missing quotes

--- models/password.js
+++ models/password.js
@@ -8,8 +8,8 @@ const passwordSchema = new mongoose.Schema({
   name: {type: String, required: true},
   password: {type: String, required: true},
   login: {type: String, required: true},
-  company: {type: mongoose.Schema.Types.ObjectId, ref: "Company"},
-  owner: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"}
+  company: {type: mongoose.Schema.Types.ObjectId, 'ref': 'Company'},
+  owner: {type: mongoose.Schema.Types.ObjectId, 'ref': 'Profile'}
 }, {
   timestamps: true
 })
