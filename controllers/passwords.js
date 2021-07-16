5af754206495605f6e8cb8b56a6ffae33ece0334
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-07-16 19:52:12 -0400

    Create controllers directory and add funcs for creating and viewing passwords

---
+++ controllers/passwords.js
@@ -0,0 +1,35 @@
+import { Password } from "../models/password.js";
+
+export {
+    newPassword as new,
+    create,
+    index
+}
+
+async function index(req, res){
+    try {
+        const passwords = await Password.find({});
+        res.render('passwords/index', {
+            title: 'Passwords',
+            passwords
+        })
+    } catch (error) {
+        console.error(error);
+        res.redirect('/passwords');
+    }
+}
+
+function newPassword(req, res){
+    res.render('passwords/new', {title: 'Add Password'});
+}
+
+async function create(req, res){
+    try {
+        req.body.owner = req.user.profile;
+        await Password.create(req.body);
+        res.redirect('/passwords/new');   
+    } catch (error) {
+        console.error(error);
+        res.redirect('/passwords/new');
+    }
+}
