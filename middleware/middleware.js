fb50a39179d38c9ddbbf031b1e06945721446ca1
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-07-19 13:57:09 -0400

    Remove console logs

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,9 +4,7 @@ export {
 }
 
 function passUserToView(req, res, next){
-    console.log("res.locals: ", res.locals);
     res.locals.user = req.user ? req.user : null;
-    console.log("res.locals after user: ", res.locals);
     next();
 }
 
