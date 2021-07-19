551115423c9e7d52300ad4468d46baec98149120
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-07-19 10:50:03 -0400

    Encrypt passwords before upload to database

--- controllers/passwords.js
+++ controllers/passwords.js
@@ -1,4 +1,5 @@
 import { Password } from "../models/password.js";
+import CryptoJS from "crypto-js";
 
 export {
     newPassword as new,
@@ -40,6 +41,9 @@ function newPassword(req, res){
 async function create(req, res){
     try {
         req.body.owner = req.user.profile;
+        let ciphertext = CryptoJS.AES.encrypt(req.body.password, req.body.masterPassword).toString();
+        req.body.password = ciphertext;
+        delete req.body.masterPassword;
         await Password.create(req.body);
         res.redirect('/passwords');   
     } catch (error) {
