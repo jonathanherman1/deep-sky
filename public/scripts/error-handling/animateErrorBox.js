605eae5699899e1a90880bb47d18baa2a3bc9683
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-07-21 19:02:55 -0400

    Renamed error handling script and made error styles available globally

--- public/scripts/error-handling/animateErrorBox.js
+++ public/scripts/error-handling/animateErrorBox.js
@@ -1,9 +1,12 @@
 const errorDiv = document.querySelector('.error-container');
 const errorDivCloseBtn = document.querySelector('#close-error-div-btn');
+const parent = errorDiv.parentElement;
 
 errorDivCloseBtn.addEventListener('click', handleClose);
 
 function handleClose(e){
     errorDiv.classList.remove('animate__fadeIn');
     errorDiv.classList.add('animate__fadeOut');
+    
+    setTimeout(()=> {parent.removeChild(errorDiv)}, 500);
 }
